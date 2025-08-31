8bcf58ab775f98261ad5723b72f8550ce2fa4731
Jason Fieldman <jason@fieldman.org>
2025-08-31 06:13:49 -0700

    project scaffold

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "ConfigPanel",
+    platforms: [.iOS(.v16), .macOS(.v14), .tvOS(.v17), .watchOS(.v7)],
+    products: [
+        .library(name: "ConfigPanel", targets: ["ConfigPanel"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jmfieldman/CombineEx.git", from: "0.0.24"),
+    ],
+    targets: [
+        .target(
+            name: "ConfigPanel",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ConfigPanelTests",
+            dependencies: ["ConfigPanel"],
+            path: "Tests"
+        ),
+    ]
+)
